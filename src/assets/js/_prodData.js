ba1f4fbf729355a87290b1d1178f56d3012a8591
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-02-08 20:21:45 +0100

    feat: prod data

---
+++ src/assets/js/_prodData.js
@@ -0,0 +1,51 @@
+export { products as default };
+
+const products = [
+  {
+    brand: "Levi's",
+    title: "RELAXED FIT TEE UNISEX",
+    originalPrice: 29.99,
+    finalPrice: 14.99,
+    tag: "Sostenibilità",
+  },
+
+  {
+    brand: "Guess",
+    title: "ROSES TEE",
+    originalPrice: 29.99,
+    finalPrice: 20.99,
+    tag: null,
+  },
+
+  {
+    brand: "Come Zucchero Filato",
+    title: "VOGLIA DI COLORI PASTELLO",
+    originalPrice: 184.99,
+    finalPrice: 129.99,
+    tag: null,
+  },
+
+  {
+    brand: "Levi's",
+    title: "TEE UNISEX",
+    originalPrice: 29.99,
+    finalPrice: 14.99,
+    tag: "Sostenibilità",
+  },
+
+  {
+    brand: "Maya Deluxe",
+    title: "STRIPE BODICE",
+    originalPrice: 29.99,
+    finalPrice: 29.99,
+    tag: null,
+  },
+
+  {
+    brand: "Esprit",
+    title: "MAGLIONE - BLACK",
+    originalPrice: 29.99,
+    finalPrice: 29.99,
+    tag: "Sostenibilità",
+  },
+];
