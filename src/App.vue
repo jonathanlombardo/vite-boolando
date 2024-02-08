32db512cc2cda45c905b5f7ebf2b6768994da24a
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-02-08 16:02:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+// import AppHeader from "./components/AppHeader.vue";
+// import AppMain from "./components/AppMain.vue";
+// import AppFooter from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      // ... mydata
+    };
+  },
+
+  methods: {
+    // ... mymethods
+  },
+
+  // components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+
+<template>
+  <!-- <app-header /> -->
+  <!-- <app-main /> -->
+  <!-- <app-footer /> -->
+</template>
+
+<style lang="scss">
+@use "./assets/css/global.scss";
+</style>
